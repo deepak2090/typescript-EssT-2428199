2040be9c392b7250831efe22b686605b0ca1cbc4
deepak2090 <deepak.2090@gmail.com>
2024-12-30 00:08:41 -0500

    update

---
+++ exercise_files/begin/src/linked.js
@@ -0,0 +1,5 @@
+var x1 = 20; // Directly assign as a number
+function saveinventory1(x) {
+    return x + 20;
+}
+console.log(saveinventory1(x1)); // Pass number
---
+++ exercise_files/begin/src/linked.ts
@@ -0,0 +1,7 @@
+let x1: any = 20;  // Directly assign as a number
+
+function saveinventory1(x: number): number {
+    return x + 20;
+}
+
+console.log(saveinventory1(x1));  // Pass number
